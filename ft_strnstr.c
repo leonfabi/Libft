3c50c9a6281afca37c2ec895314a7c4ca5316405
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-03-24 18:58:22 +0100

    updates

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:56:30 by singerporsc       #+#    #+#             */
-/*   Updated: 2023/03/24 14:09:28 by fkrug            ###   ########.fr       */
+/*   Updated: 2023/03/24 18:57:45 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,20 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 
 	index1 = 0;
 	index2 = 0;
-	while (index1 < len && s1[index1 + index2])
+	if (*s2)
+		return ((char *)s1);
+	else
 	{
-		while (s1[index1 + index2] == s2[index2] && (index1 + index2) < len)
-			index2++;
-		if (index2 == ft_strlen(s2))
-			return ((char *) &s1[index1]);
-		else
-			index2 = 0;
-		index1++;
+		while (index1 < len && s1[index1 + index2])
+		{
+			while (s1[index1 + index2] == s2[index2] && (index1 + index2) < len)
+				index2++;
+			if (index2 == ft_strlen(s2))
+				return ((char *) &s1[index1]);
+			else
+				index2 = 0;
+			index1++;
+		}
+		return (NULL);
 	}
-	return (NULL);
 }
