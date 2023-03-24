7992ce9fc789471d54a8e7125987ff4b2ee76826
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-03-24 18:41:29 +0100

    fixed

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: fkrug <fkrug@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 16:43:10 by singerporsc       #+#    #+#             */
-/*   Updated: 2023/03/24 14:19:24 by fkrug            ###   ########.fr       */
+/*   Updated: 2023/03/24 18:40:24 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned const char	*pointer;
 
 	pointer = s;
-	while (n > 0 && *pointer != c)
+	if (n > 0)
 	{
-		pointer++;
-		n--;
+		while (n > 1 && *pointer != (unsigned char)c)
+		{
+			pointer++;
+			n--;
+		}
+		if (*pointer == (unsigned char)c)
+			return ((char *) pointer);
 	}
-	if (*pointer == c)
-		return ((char *) pointer);
 	return (NULL);
 }
