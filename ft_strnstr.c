24ee732e862838d40b6660b7cd0160b6f2d0f5a4
fabileo <76965192+leonfabi@users.noreply.github.com>
2023-03-27 19:15:39 +0200

    fixed

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,37 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fkrug <fkrug@student.42.fr>                +#+  +:+       +#+        */
+/*   By: singerporsche <singerporsche@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/20 11:56:30 by singerporsc       #+#    #+#             */
-/*   Updated: 2023/03/24 18:57:45 by fkrug            ###   ########.fr       */
+/*   Created: 2023/03/27 18:17:54 by singerporsc       #+#    #+#             */
+/*   Updated: 2023/03/27 19:14:30 by singerporsc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-	size_t		index1;
-	size_t		index2;
+	size_t	ib;
+	size_t	il;
 
-	index1 = 0;
-	index2 = 0;
-	if (*s2)
+	ib = 0;
+	il = 0;
+	if (!*s2)
 		return ((char *)s1);
-	else
+	if (len > 0)
 	{
-		while (index1 < len && s1[index1 + index2])
+		while (s1[ib] && ib < len)
 		{
-			while (s1[index1 + index2] == s2[index2] && (index1 + index2) < len)
-				index2++;
-			if (index2 == ft_strlen(s2))
-				return ((char *) &s1[index1]);
+			while (s1[ib + il] == s2[il] && (il + ib) < len && s1[ib + il])
+				il++;
+			if (il == ft_strlen(s2))
+				return ((char *)&(s1[ib]));
 			else
-				index2 = 0;
-			index1++;
+				il = 0;
+			ib++;
 		}
-		return (NULL);
 	}
+	return (NULL);
 }
