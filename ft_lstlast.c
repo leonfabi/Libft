e13e8aeb23586348e559dc64797605eb15b08ebd
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-03-28 14:29:27 +0200

    fixed listlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -5,10 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fkrug <fkrug@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/23 16:33:53 by singerporsc       #+#    #+#             */
-/*   Updated: 2023/03/24 14:21:34 by fkrug            ###   ########.fr       */
+/*   Created: 2023/03/23 16:33:53 by fkrug             #+#    #+#             */
+/*   Updated: 2023/03/28 14:29:18 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "libft.h"
 
@@ -17,6 +18,8 @@ t_list	*ft_lstlast(t_list *lst)
 	t_list	*counter;
 
 	counter = lst;
+	if (counter == NULL)
+		return (NULL);
 	while (counter->next)
 		counter = counter->next;
 	return (counter);
