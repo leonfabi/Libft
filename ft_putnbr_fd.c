92fd2ad87b184680c3fd3cfaba192ab25459c86b
42fabileo <76965192+leonfabi@users.noreply.github.com>
2023-03-28 16:38:58 +0200

    fixed header issue

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,20 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: singerporsche <singerporsche@student.42    +#+  +:+       +#+        */
+/*   By: fkrug <fkrug@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/20 11:47:39 by singerporsc       #+#    #+#             */
-/*   Updated: 2023/03/20 12:51:39 by singerporsc      ###   ########.fr       */
+/*   Created: 2023/03/20 11:47:39 by fkrug             #+#    #+#             */
+/*   Updated: 2023/03/28 16:36:25 by fkrug            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
+
+int	ft_l(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		count++;
+	while (n)
+	{
+		count++;
+		n /= 10;
+	}
+	return (count);
+}
+
+//END
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
+	int		length;
+	char	str[12];
+	size_t	nbr;
 
-	str = ft_itoa(n);
+	nbr = n;
+	ft_bzero(str,12);
+	length = ft_l(n);
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		nbr = -n;
+	}
+	while (nbr)
+	{
+		str[length - 1] = nbr % 10 + '0';
+		nbr /= 10;
+		length--;
+	}
 	ft_putstr_fd(str, fd);
 }
+/*int	main(void)
+{
+	ft_putnbr_fd(2,1);
+	return (0);
+}*/
